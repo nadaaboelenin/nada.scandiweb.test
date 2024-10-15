0f0dfa87ce2675961935b7022a819f6816a88510
nadaaboelenin <nada.aboelenin@gmail.com>
2024-10-15 11:50:10 +0300

    add config

---
+++ public_html/config.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "localhost";
+$username = "your_db_user";
+$password = "your_db_password";
+$dbname = "your_database_name";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
